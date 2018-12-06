7a4e3da8db74f02205331cacea1e7d66e0ca5a91
qiratmerchant <qirat@fleekbiz.com>
2018-12-06 18:47:17 +0500

    UI Fixes

--- application/views/package.php
+++ application/views/package.php
@@ -1,49 +1,45 @@
 <section>
    <div class="">
       <div class="">
-         <div class="col-md-3 bg_color">
+         <div class="col-md-3 bg_color bg_color1">
             <h1 class="h_content">5/6
                <br>Select
                <br>Package
             </h1>
-            <p class="p_content">Lorem ipsum dolor sit amet,<br>consectetur adipiscing elit,<br> sed to eiusmod tempor<br> incididunt ut labore et <br>dolore magna aliqua.</p>
+            <p class="p_content">Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed to eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
          </div>
-         <div class="col-md-9 bg1_height">
-            <?php foreach($packages as $package){ ?>
-            <div class="col-md-6">
-               <h2 class="s_main"><?= $package['name'] ?> Packages</h2>
-               <div class="box">
-                  <div class="icon">
-                     <!--<div class="image"><i class="fa fa-thumbs-o-up"></i></div>-->
-                     <div class="info i_info">
-                        <ul>
-                          <?= $package['package_details'] ?>
-                        </ul>
-                        <h2 class="n_num">$<?= $package['package_price'] ?></h2>
-                        <p class="o_one">One time cost</p>
-                        <div class="more">
-                           <a href="#" onclick="packageSelect(<?= $package['package_id']?>)" title="Title Link">
-                            SELECT 
-                           </a>
+         <div class="col-md-9 padding_bg">
+            <div class="topSection top_one">
+			  <?php foreach($packages as $package){ ?>
+               <div class="col-md-6">
+                  <h2 class="s_main"><?= $package['name'] ?> Package</h2>
+                  <div class="box">
+                     <div class="icon">
+                        <div class="info i_info">
+						  <ul>
+                           <?= $package['package_details'] ?>
+						  </ul>
+                           <h2 class="n_num">$<?= $package['package_price'] ?></h2>
+                           <p class="o_one">One time cost</p>
+                           <div class="more">
+                              <a href="#" class="button-default btn" onclick="packageSelect(<?= $package['package_id']?>)" title="Title Link">SELECT</a>
+                           </div>
                         </div>
                      </div>
                   </div>
                </div>
+			   <?php } ?>
+			   <input type="hidden" name="packageid" id="packageid" >
             </div>
-            <?php } ?>
-            <input type="hidden" name="packageid" id="packageid" >
             <footer>
                <div class="footer_inner_left clearifix">
                   <a href="<?= base_url();?>other-info" class="c_back"><span class="glyphicon glyphicon-arrow-left"></span>Back</a>
                </div>
                <div class="footer_inner clearifix">
-                   <a href="#" onclick="SaveChanges5()" class="c_continue"><span class="text">Continue</span> <span class="glyphicon glyphicon-arrow-right"></span></a>
+                  <a href="#" onclick="SaveChanges5()" class="c_continue"><span class="text">Continue</span> <span class="glyphicon glyphicon-arrow-right"></span></a>
                </div>
+            </footer>
          </div>
-         </footer>  
-         <!--<a href="otherinfo.html" class="con_back"><span class="glyphicon glyphicon-arrow-left"></span>Back</a>
-            <a href="confirm_purchase.html" class="con_continue">Continue<span class="glyphicon glyphicon-arrow-right"></span></a>-->
       </div>
-   </div>
    </div>
 </section>
--- application/views/theme.php
+++ application/views/theme.php
@@ -1,88 +1,65 @@
 <section>
-<div class="">
-<div class="">
-<div class="col-md-3 bg_color">
-<h1 class="h_content">3/6
-<br>Select
-<br>Theme
-</h1>
-<p class="p_content">Lorem ipsum dolor sit amet,<br>consectetur adipiscing elit,<br> sed to eiusmod tempor<br> incididunt ut labore et <br>dolore magna aliqua.</p>
-</div>
-<div class="col-md-9 bg1_height">
-
-<div class="topSection">
-
-<div class="row" style=" margin:20px">
-<div class="col-xs-12">
-<button class="btn btn-small btn-danger btn_color" data-toggle="portfilter" data-target="all">
-All
-</button>
-<?php foreach($categories as $category){ ?> 
-<button class="btn btn-small btn-danger btn_color" data-toggle="portfilter" data-target="<?= $category['tag']?>">
-<?= $category['name'] ?>
-</button>
-<?php }  ?>
-</div>
-</div>
-<div class="row">
-<?php  foreach($themes as $theme){ ?>
-<div class="col-md-3" data-tag='cms'>
-<div class="s_shadow">
-<h1 class="alignleft"><?= strtoupper($theme['theme_name']) ?></h1>
-<a href="#" onclick="themeSelect(<?= $theme['theme_id'] ?>)" class="alignright">SELECT</a>
-<img src="<?=base_url()?>assets/front/images/<?= $theme['image1'] ?>" class="img-responsive i_img">
-
-</div>
-</div>
-<?php } ?>
-</div>
-<input type="hidden" name="theme_id" id="theme_id">
-</div>
-<!--<div class="col-md-3">
-<div class="s_shadow">
-<h1 class="alignleft">THEME 1</h1>
-<a href="#" class="alignright">SELECT</a>
-<img src="images/image1.jpg" class="img-responsive i_img">
-</div>
-</div>
-
-<div class="col-md-3">
-<div class="s_shadow">
-<h1 class="alignleft">THEME 2</h1>
-<a href="#" class="alignright">SELECT</a>
-<img src="images/image2.jpg" class="img-responsive i_img">
-</div>
-</div>
-
-<div class="col-md-3">
-<div class="s_shadow">
-<h1 class="alignleft">THEME 3</h1>
-<a href="#" class="alignright">SELECT</a>
-<img src="images/image3.jpg" class="img-responsive i_img">
-</div>
-</div>
-
-<div class="col-md-3">
-<div class="s_shadow">
-<h1 class="alignleft">THEME 4</h1>
-<a href="#" class="alignright">SELECT</a>
-<img src="images/image3.jpg" class="img-responsive i_img">
-</div>
-</div>-->
-<footer>
-<div class="footer_inner_left clearifix">
-<a href="<?= base_url();?>select-domain" class="c_back"><span class="glyphicon glyphicon-arrow-left"></span>Back</a>
-</div>
-<div class="footer_inner clearifix">
-
-<a href="#" onclick="SaveChanges3()" class="c_continue"><span class="text">Continue</span> <span class="glyphicon glyphicon-arrow-right"></span></a>
-
-</div>
-</div>
-</footer>  
-<!--<a href="selectdomain.html" class="con_back"><span class="glyphicon glyphicon-arrow-left"></span>Back</a>
-<a href="otherinfo.html" class="con_continue">Continue<span class="glyphicon glyphicon-arrow-right"></span></a>-->
-</div>
-</div>
-</div>
+   <div class="">
+   <div class="">
+      <div class="col-md-3 bg_color bg_color1">
+         <h1 class="h_content">3/6
+            <br>Select
+            <br>Theme
+         </h1>
+         <p class="p_content">Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed to eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+      <div class="col-md-9 bg1_height">
+         <div class="topSection">
+            <div class="row">
+               <div class="col-xs-12">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  </button>
+                  <!-- Collect the nav links, forms, and other content for toggling -->
+                  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                     <button class="btn btn-small btn-danger btn_color" data-toggle="portfilter" data-target="all">
+                     All
+                     </button>
+					 <?php
+					   foreach($categories as $category){ ?>
+					   <button class="btn btn-small btn-danger btn_color" data-toggle="portfilter" data-target="<?= $category['tag']?>"><?= $category['name'] ?></button>
+					 <?php }  ?>
+                  </div>
+               </div>
+            </div>
+            <div class="theme-sec">
+               <div class="row">
+			    <?php  foreach($themes as $theme){ ?>
+                  <div class="theme-box" data-tag='cms<!-- Categrory tag here.. Dynamic -->'>
+                     <div class="col-md-3">
+                        <div class="panel">
+                           <div class="s_shadow">
+                              <h1 class="alignleft"><?= strtoupper($theme['theme_name']) ?></h1>
+                              <a href="#" onclick="themeSelect(<?= $theme['theme_id'] ?>)" class="alignright">SELECT</a>
+                              <a href="<?=base_url()?>assets/front/images/<?= $theme['image1'] ?>" class="thumbnail " data-fancybox>
+								<img src="<?=base_url()?>assets/front/images/<?= $theme['image1'] ?>" class="img-responsive i_img">
+                              </a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+				<?php } ?>      
+			   </div>
+            </div>
+			<input type="hidden" name="theme_id" id="theme_id">
+            <div class="clearifix"></div>
+         </div>
+         <footer>
+            <div class="footer_inner_left clearifix">
+               <a href="<?= base_url();?>select-domain" class="c_back"><span class="glyphicon glyphicon-arrow-left"></span><span class="text">Back</span></a>
+            </div>
+            <div class="footer_inner clearifix">
+               <a href="#" onclick="SaveChanges3()" class="c_continue"><span class="text">Continue</span> <span class="glyphicon glyphicon-arrow-right"></span></a>
+            </div>
+         </footer>
+      </div>
+   </div>
 </section>
