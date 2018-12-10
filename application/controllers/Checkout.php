e24617f3ca1e7368f8394f09d3d9e52210ca95e3
azeemfleekbiz <35449155+azeemfleekbiz@users.noreply.github.com>
2018-12-10 19:30:24 +0500

    email
    send email

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -16,10 +16,6 @@
 		
 		public function checkoutsubmit()
 		{
-				print_r($_POST);
-				die;
-			// $_COOKIE['userinfo'].','.$_POST['phone'].','.$_POST['email'].','.$_POST['rad']
-			// set()
 			$cookiearr=explode(',', $_COOKIE['userinfo']);
 				
 				require_once "vendor\stripe\stripe-php\init.php";
@@ -48,21 +44,43 @@
 					$status=0;
 				}
 			$cookiearr=explode(',', $_COOKIE['userinfo']);
-				print_r($cookiearr);
-				die;
-			$userdata=array('email'=>$cookiearr[9],'status'=>0,'date_created'=>DateTime_Now);
+			$password=rand(100000,999999);	
+			$userdata=array('email'=>$_POST['email'],'password'=>$password,'status'=>0,'date_created'=>DateTime_Now);
 			$userid=$this->Dmodel->insertdatatoid('pre_users',$userdata);
-			$userdetailsdata=array('user_id'=>$userid,'phone'=>$cookiearr[8]);
+			$userdetailsdata=array('user_id'=>$userid,'phone'=>$_POST['phone']);
 			$usdetail=$this->Dmodel->insertdata('pre_user_details',$userdetailsdata);
-			$webdata=array('user_id'=>$userid,'business_name'=>$cookiearr[0],'industry_id'=>$cookiearr[1],'business_logo'=>$cookiearr[2],'have_domain'=>$cookiearr[3],'domain'=>$cookiearr[4],'theme_id'=>$cookiearr[5],'customization_details'=>$cookiearr[6],'package_id'=>$cookiearr[7],'contact_preference'=>$cookiearr[10],'status'=>0,'date_created'=>DateTime_Now);
+			$webdata=array('user_id'=>$userid,'business_name'=>$cookiearr[0],'industry_id'=>$cookiearr[1],'business_logo'=>$cookiearr[2],'have_domain'=>$cookiearr[3],'domain'=>$cookiearr[4],'theme_id'=>$cookiearr[5],'customization_details'=>$cookiearr[6],'package_id'=>$cookiearr[7],'contact_preference'=>$_POST['optradio'],'status'=>0,'date_created'=>DateTime_Now);
 			$packagewhere=array('package_id'=>$cookiearr[7]);
 			$packages=$this->Dmodel->get_tbl_whr_arr('pre_packages',$packagewhere);
 			$amount=$_POST['amount'];
 			$webid=$this->Dmodel->insertdatatoid('pre_webapp',$webdata);
-			$paymentarr=array('user_id'=>$userid,'webapp_id'=>$webid,'amount_paid'=>$amount,'status'=>$status,'payment_date'=>DateTime_Now,'gateway'=>'stripe','gateway_returns'=>$cookiearr[11]);
+			$paymentarr=array('user_id'=>$userid,'webapp_id'=>$webid,'amount_paid'=>$amount,'status'=>$status,'payment_date'=>DateTime_Now,'gateway'=>'stripe','gateway_returns'=>$charge->id);
 			$webid=$this->Dmodel->insertdatatoid('pre_payments',$paymentarr);
-
 			$viewdata['message']=$message;
+
+				$maildata['from_email']='test@yopmail.com';
+				$maildata['from_name']='tesdt';
+				$maildata['to_email']='saad@yopmail.com';
+				$maildata['to_name']='saad';
+				$maildata['subject']='hello';
+
+				$maildata['message']='Hi {{User Name}},
+We welcome you aboard!
+Thank you for placing your order at '.Site_Title.'. Please find your account credentials below:
+Username: '.$_POST['email'].'
+
+
+Password: '.$password.'
+Please click here to login your account. Enter the given user name and password and you are good to go!
+
+Warm regards,
+
+
+'.Site_Title.'Customer Support';
+				$mail=$this->Dmodel->send_mail($maildata);
+				print_r($mail);
+				die;
+
 			redirect(base_url().'payment-confirm',$viewdata);
 
 		}
